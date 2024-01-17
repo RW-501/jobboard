fe72ac39ebc50089b96ed8d090db9d23256ead37
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-01-17 11:43:30 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,16 @@
 // auth.js
 
-
-/*
-// Function to log in with email and password
- const loginWithEmailPassword = async (email, password) => {
-  try {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    return true;
-  } catch (error) {
-    console.error('Login failed:', error.message);
-    return false;
-  }
-};
-*/
-
 // Function to log out the user
- const logout = async () => {
+const logout = async () => {
   try {
     await firebase.auth().signOut();
+    removeLoggedInFlag(); // Remove the flag from localStorage on logout
   } catch (error) {
     console.error('Logout failed:', error.message);
   }
 };
-/*
-// Function to register a new user with email and password
- const registerWithEmailPassword = async (email, password) => {
-  try {
-    const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    // Create a user document in Firestore
-    await firebase.firestore().collection('users').doc(userCredential.user.uid).set({
-      email: email,
-      // Add more user-related data here
-    });
-    return true;
-  } catch (error) {
-    console.error('Registration failed:', error.message);
-    return false;
-  }
-};
-*/
+
 // Function to check if a user is logged in
- var checkUserLogin = () => {
+var checkUserLogin = () => {
   return firebase.auth().currentUser;
 };
